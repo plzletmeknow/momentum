d819b1151f9bd1e09c22487c6f1823a9ae5c7b3c
plzletmeknow <97961088+plzletmeknow@users.noreply.github.com>
2022-03-10 22:02:43 +0900

    JS-event-study

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-function sayHello(nameOfPerson, age) {
-  console.log("Hello my name is " + nameOfPerson + " and i'm " + age);
-}
-sayHello("nico", 10);
+const title = document.querySelector("body h2");
+
+const superEventHandler = {
+  handleMouseEnter: function () {
+    title.innerText = "The mouse is here!";
+  },
+  handleMouseLeave: function () {
+    title.innerText = "The mouse is gone!";
+  },
+  handleWindowResize: function () {
+    title.innerText = "You just resized!";
+  },
+  handleMouseRightClick: function () {
+    title.innerText = "That was a right click!";
+  },
+};
+
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+window.addEventListener("resize", superEventHandler.handleWindowResize);
+window.addEventListener("contextmenu", superEventHandler.handleMouseRightClick);
